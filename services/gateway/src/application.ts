0efef5cd1c99cb05141db2a8d427a6e964262a00
Mateusz Odelga <mateusz.odelga@schibsted.com>
2020-09-21 05:52:34 +0200

    Support partial success code on response

--- services/gateway/src/application.ts
+++ services/gateway/src/application.ts
@@ -7,14 +7,30 @@ export type ConfigType = {
   PORT: number;
 };
 
-function translateError(code: number) {
+function translateCode(code: number) {
   switch (code) {
+    case 200:
+      return "ok";
+    case 206:
+      return "partial-ok";
     case 401:
       return "invalid_login";
     case 403:
       return "session_expired";
     default:
       return "scrape_failed";
+  }
+}
+
+function successStatusCode(errors: string[], succeed: string[]) {
+  if (errors.length === 0) {
+    return 200;
+  }
+  if (errors.length > 0 && succeed.length !== 0) {
+    return 204;
+  }
+  if (errors.length > 0 && succeed.length === 0) {
+    throw new HttpError(500, `Logged in but scraper failed on: ${errors.join(",")}`);
   }
 }
 
@@ -33,9 +49,9 @@ export function start({ PORT }: ConfigType) {
         console.log(username, password);
         throw new HttpError(400, "Bad Request: inusufficient login data");
       }
-      const { errors, ...data } = await scrape(username, password);
-      // errors.filter()
-      res.json({ status: "ok", ...data });
+      const { succeed, errors, ...data } = await scrape(username, password);
+      const code = successStatusCode(errors, succeed);
+      res.json({ status: translateCode(code), ...data });
     } catch (e) {
       next(e);
     }
@@ -45,10 +61,10 @@ export function start({ PORT }: ConfigType) {
     console.error(err);
     if (err instanceof HttpError) {
       res.status(err.code);
-      res.json({ status: "error", error: translateError(err.code), message: err.message });
+      res.json({ status: "error", error: translateCode(err.code), message: err.message });
     } else {
       res.status(500);
-      res.json({ status: "error", error: translateError(500) });
+      res.json({ status: "error", error: translateCode(500) });
     }
   });
 
